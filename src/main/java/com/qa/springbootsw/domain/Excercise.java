3b0a71856cf2ae4fb6220a4d3f9ae08e89cd632d
eamonmoradi <eamonmoradi@192.168.0.20>
2021-11-16 15:26:43 +0000

    Creating Excercise domain

--- src/main/java/com/qa/springbootsw/domain/Excercise.java
+++ src/main/java/com/qa/springbootsw/domain/Excercise.java
@@ -1,9 +1,24 @@
 package com.qa.springbootsw.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Excercise {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long excerciseID;
+	
+	@Column(nullable = false)
+	private String excerciseName;
+	
+	@Column(nullable = false)
+	private String muscleTargeted;
+	
 	
 
 }
