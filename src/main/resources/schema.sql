94d47372751c3e04efa781ba9c227f57796dd8b2
Eamon Moradi <eamonmoradi@Eamons-MacBook-Air.local>
2021-11-17 15:51:56 +0000

    adding SQL schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+	DROP TABLE IF EXISTS `Excercise` CASCADE;
+CREATE TABLE `Excercise`(
+	`excerciseID` BIGINT PRIMARY KEY AUTO_INCREMENT,
+	`excerciseName` VARCHAR(255) NOT NULL,
+	`muscleTargeted` VARCHAR(255) NOT NULL),noOfReps INT(30)
+	`noOfSets` INT(30),
+	`sessionID` BIGINT FOREIGN KEY REFERENCES WorkoutPlanDB(sessionID));
+	
+	DROP TABLE IF EXISTS `WorkoutPlanDB` CASCADE;
+	CREATE TABLE `workoutPlanDB`(
+	`sessionID` BIGINT PRIMARY KEY AUTO_INCREMENT,
+	`dayOfWeek` VARCHAR(14),
+	`excerciseID` BIGINT FOREIGN KEY REFERENCES Excercise(excerciseID)
+	
+	);
+	
+	
