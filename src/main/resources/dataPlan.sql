7b485a576676edba298a01e4a4fcc761a97024d1
Eamon Moradi <eamonmoradi@Eamons-MacBook-Air.local>
2021-11-17 16:22:13 +0000

    creating sql files

---
+++ src/main/resources/dataPlan.sql
@@ -0,0 +1,13 @@
+INSERT into Excercise (excerciseName,muscleTargeted,noOfReps,noOfSets) Values ("dumbell curl","bicep",12,4);
+INSERT into Excercise (excerciseName,muscleTargeted,noOfReps,noOfSets) Values("bench press","chest",5,5); 
+INSERT into Excercise (excerciseName,muscleTargeted,noOfReps,noOfSets) Values("dumbell press","chest",7,4);
+INSERT into Excercise (excerciseName,muscleTargeted,noOfReps,noOfSets) Values("rope pull downs","tricep",15,4);
+INSERT into Excercise (excerciseName,muscleTargeted,noOfReps,noOfSets) Values("preacher curl","bicep",10,5);
+INSERT into Excercise (excerciseName,muscleTargeted,noOfReps,noOfSets) Values
+
+
+INSERT into WorkoutPlanDB (dayOfWeek,excerciseID) VALUES ("monday",(1,3,4,2));
+INSERT into WorkoutPlanDB (dayOfWeek,excerciseID) VALUES("thursday",(2,5,3));
+INSERT into WorkoutPlanDB (dayOfWeek,excerciseID) VALUES("friday",(4,5,6);
+INSERT into WorkoutPlanDB (dayOfWeek,excerciseID) VALUES
+INSERT into WorkoutPlanDB (dayOfWeek,excerciseID) VALUES
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,16 +2,17 @@
 CREATE TABLE `Excercise`(
 	`excerciseID` BIGINT PRIMARY KEY AUTO_INCREMENT,
 	`excerciseName` VARCHAR(255) NOT NULL,
-	`muscleTargeted` VARCHAR(255) NOT NULL),noOfReps INT(30)
+	`muscleTargeted` VARCHAR(255) NOT NULL,
+    `noOfReps` INT(30),
 	`noOfSets` INT(30),
-	`sessionID` BIGINT FOREIGN KEY REFERENCES WorkoutPlanDB(sessionID));
+	`sessionID` BIGINT FOREIGN KEY REFERENCES WorkoutPlanDB(sessionID)
+    );
 	
 	DROP TABLE IF EXISTS `WorkoutPlanDB` CASCADE;
 	CREATE TABLE `workoutPlanDB`(
 	`sessionID` BIGINT PRIMARY KEY AUTO_INCREMENT,
 	`dayOfWeek` VARCHAR(14),
 	`excerciseID` BIGINT FOREIGN KEY REFERENCES Excercise(excerciseID)
-	
 	);
 	
 	
